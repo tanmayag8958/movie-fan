2d693867c90686aac0648a38bb5b6190bb58281b
Tanmay Agarwal <tanmay.agarwal@vphrase.com>
2023-11-04 04:54:25 +0530

    ADDED: Vue 3 with chart.js and vue-chartjs

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <DashboardViewer/>
+</template>
+
+<script>
+import DashboardViewer from './components/Dashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DashboardViewer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,81 @@
+<template>
+    <Line :data="data" :options="options"/>
+</template>
+
+<script>
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import {Line} from 'vue-chartjs'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default {
+    name: 'DashboardViewer',
+    components: {
+        Line
+    },
+    data() {
+        return {
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [
+                    {
+                        label: 'Data One',
+                        backgroundColor: '#f87979',
+                        pointRadius: 0,
+                        tension: 0.3,
+                        data: [40, 39, 10, 40, 39, 80, 40]
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        ticks: {
+                            color: "#b6baca",
+                        },
+                        grid: {
+                            drawTicks: false,
+                        },
+                        border: {
+                            dash: [5, 10],
+                        },
+                    },
+                    x: {
+                        ticks: {
+                            color: "#b6baca",
+                        },
+                        grid: {
+                            display: false,
+                        },
+                        border: {
+                            display: false,
+                        },
+                    },
+                },
+                interaction: {
+                    intersect: false,
+                },
+                borderColor: "#c319ee",
+                backgroundColor: "#c319ee",
+            },
+        }
+    }
+}
+</script>
